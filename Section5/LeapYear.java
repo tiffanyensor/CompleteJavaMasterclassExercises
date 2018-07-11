25c9ffbf54f2fb979b435de29795b1aadd1c0495
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 12:18:36 -0400

    Update LeapYear.java
    fixed indent

--- Section5/LeapYear.java
+++ Section5/LeapYear.java
@@ -12,7 +12,7 @@ package com.company;
 public class LeapYear {
 
     public static void main(String[] args) {
-	    System.out.println(isLeapYear(-1600));
+	System.out.println(isLeapYear(-1600));
         System.out.println(isLeapYear(1600));
         System.out.println(isLeapYear(2017));
         System.out.println(isLeapYear(2000));
