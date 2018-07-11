6cb99363732b92d4923879567c3218f04822cba1
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 14:25:05 -0400

    Add files via upload

---
+++ Section6/Main.java
@@ -0,0 +1,37 @@
+/*
+Write a method named printFactors with one parameter of type int named number. 
+If number is < 1, the method should print "Invalid Value".
+The method should print all factors of the number.
+A factor of a number is an integer which divides that number wholly (i.e. without leaving a remainder).
+For example: 3 is a factor of 6 because 3 fully divides 6 without leaving a remainder.
+In other words 6 / 3 = 2.
+ */
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+    }
+
+    public static void printFactors(int number){
+
+        // validate input
+        if (number < 1){
+            System.out.println("Invalid Value");
+        }
+
+        // print factors
+        for (int i=1; i<=number; i++){
+            if (number % i == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}
