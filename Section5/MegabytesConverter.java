8c5ff11f3eb1122f94e5c73241070f97dcced7b9
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 12:08:26 -0400

    Update MegabytesConverter.java
    instructions added

--- Section5/MegabytesConverter.java
+++ Section5/MegabytesConverter.java
@@ -1,3 +1,11 @@
+/*
+Write a method called printMegaBytesAndKiloBytes that has 1 parameter of type int with the name kiloBytes.
+The method should not return anything (void) and it needs to calculate the megabytes and remaining kilobytes from the kilobytes parameter. 
+Then it needs to print a message in the format "XX KB = YY MB and ZZ KB". XX represents the original value kiloBytes.
+YY represents the calculated megabytes. ZZ represents the calculated remaining kilobytes.
+If the parameter kiloBytes is < 0 then print instead the text "Invalid Value". 
+*/
+
 package com.company;
 
 public class Main {
