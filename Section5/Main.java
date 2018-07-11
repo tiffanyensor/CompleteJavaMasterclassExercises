c889d9a8eda6bf082611d7559bd7a12ad6c95153
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 12:14:53 -0400

    Add files via upload

---
+++ Section5/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
+        num1 = (int) (num1*1000);
+        num2 = (int) (num2*1000);
+        if (num1 == num2){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+}
