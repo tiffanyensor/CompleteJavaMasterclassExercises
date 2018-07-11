e086f75fcd107831decb7190870b83f1da55b082
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 12:10:56 -0400

    Update BarkingDog.java
    instructions added

--- Section5/BarkingDog.java
+++ Section5/BarkingDog.java
@@ -1,6 +1,16 @@
+/*
+We have a dog that likes to bark.  We need to wake up if the dog is barking at night!
+Write a method bark that has 2 parameters.
+1st parameter should be of type boolean and be named barking it represents if our dog is currently barking. 
+2nd parameter represents the hour of the day and is of type int with the name hourOfDay and has valid range of 0-23.
+We have to wake up if the dog is barking before 08:00 am or after 22:00 hours so in that case return true. 
+In all other cases return false. If the hourOfDay parameter is less than 0 or greater than 23 return false.
+*/
+
+
 package com.company;
 
-public class Main {
+public class BarkingDog {
 
     public static void main(String[] args) {
         System.out.println(bark(true, 1));
