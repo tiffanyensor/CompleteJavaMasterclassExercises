5bd18346c81d12494ca09748d5bb5946cd37b938
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 12:34:30 -0400

    Update and rename Main.java to PlayingCat.java

--- Section5/PlayingCat.java
+++ Section5/PlayingCat.java
@@ -1,6 +1,17 @@
+/*
+The cats spend most of the day playing. 
+In particular, they play if the temperature is between 25 and 35 (inclusive). 
+Unless it is summer, then the upper limit is 45 (inclusive) instead of 35.
+
+Write a method isCatPlaying that has 2 parameters. 
+Method needs to return true if cat is playing, otherwise return false
+1st parameter should be of type boolean and be named summer it represents if it is summer. 
+2nd parameter represents the temperature and is of type int with the name temperature.
+*/
+
 package com.company;
 
-public class Main {
+public class PlayingCat {
 
     public static void main(String[] args) {
         System.out.println(isCatPlaying(true, 10));
