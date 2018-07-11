0286abc8efb382b661ffbe43ee8685babd6a70ef
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 12:25:39 -0400

    Update and rename Main.java to AreaCalculator.java

--- Section5/AreaCalculator.java
+++ Section5/AreaCalculator.java
@@ -1,6 +1,15 @@
+/*
+Write a method named area with one double parameter named radius.
+The method needs to return a double value that represents the area of a circle.
+If the parameter radius is negative then return -1.0 to represent an invalid value. 
+Write another overloaded method with 2 parameters x and y (both doubles), where x and y represent the sides of a rectangle.
+The method needs to return an area of a rectangle. 
+If either or both parameters is/are negative return -1.0 to indicate an invalid value.
+*/
+
 package com.company;
 
-public class Main {
+public class AreaCalculator {
 
     public static void main(String[] args) {
         area(5.0);
