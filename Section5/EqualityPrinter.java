42eb4d13e15ea14cc042985205ea4300d41416f5
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 12:31:58 -0400

    Update and rename Main.java to EqualityPrinter.java

--- Section5/EqualityPrinter.java
+++ Section5/EqualityPrinter.java
@@ -1,6 +1,14 @@
+/*
+Write a method printEqual with 3 parameters of type int. The method should not return anything (void).
+If one of parameters is less than 0, print text "Invalid Value".
+If all numbers are equal print text "All numbers are equal"
+If all numbers are different print text "All numbers are different".
+Otherwise print "Neither all are equal or different".
+*/
+
 package com.company;
 
-public class Main {
+public class EqualityPrinter {
 
     public static void main(String[] args) {
         printEqual(1,1,1);
