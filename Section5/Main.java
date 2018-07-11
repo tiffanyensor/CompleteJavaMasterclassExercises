ed86398c0cd1b01bd12cd760ba94b10746b1e103
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 12:12:36 -0400

    Add files via upload

---
+++ Section5/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+    }
+
+
+    public static boolean isLeapYear(int year){
+        if (year < 1 || year > 9999){
+            return false;
+        }
+        if (year % 400 == 0){
+            return true;
+        }
+        else if ((year % 4 == 0) && (year % 100 != 0)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+
+}
