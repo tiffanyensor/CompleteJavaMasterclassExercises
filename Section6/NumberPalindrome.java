b57b7ad0ef28e2200a003ddfc2872771f9de5576
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 14:05:07 -0400

    Update and rename Main.java to NumberPalindrome.java

--- Section6/NumberPalindrome.java
+++ Section6/NumberPalindrome.java
@@ -6,7 +6,7 @@ otherwise it should return false.  
 
 package com.company;
 
-public class Main {
+public class NumberPalindrome {
 
     public static void main(String[] args) {
 	    System.out.println(isPalindrome(45));
