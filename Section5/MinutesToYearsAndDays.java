c3df36e41ba9be95a4646f58238d78ba08d45b98
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 12:29:18 -0400

    Update and rename Main.java to MinutesToYearsAndDays.java

--- Section5/MinutesToYearsAndDays.java
+++ Section5/MinutesToYearsAndDays.java
@@ -1,6 +1,15 @@
+/*
+Write a method printYearsAndDays with parameter of type long named minutes.
+The method should not return anything (void) and it needs to calculate the years and days from the minutes parameter.
+If parameter is less than 0, print text "Invalid Value".
+Otherwise if parameter is valid then it needs to print a message in the format "XX min = YY y and ZZ d".
+XX represents the original value minutes. YY represents the calculated years. ZZ represents the calculated days.
+*/
+
+
 package com.company;
 
-public class Main {
+public class MinutesToYearsAndDays {
 
     public static void main(String[] args) {
         printYearsAndDays(-4);
