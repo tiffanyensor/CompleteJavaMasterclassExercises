d1a987c07575434f979e94af440e375e9d155cb2
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 12:13:47 -0400

    Update and rename Main.java to LeapYear.java
    title and instructions

--- Section5/LeapYear.java
+++ Section5/LeapYear.java
@@ -1,8 +1,17 @@
+/*
+Write a method isLeapYear with a parameter of type int named year. 
+The parameter needs to be greater than or equal to 1 and less than or equal to 9999.
+If the parameter is not in that range return false. 
+Otherwise, if it is in the valid range, calculate if the year is a leap year and return true if it is, otherwise return false. 
+A year is a leap year if it is divisible by 4 but not by 100, or it is divisible by 400.
+*/
+
+
 package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void LeapYear(String[] args) {
 	    System.out.println(isLeapYear(-1600));
         System.out.println(isLeapYear(1600));
         System.out.println(isLeapYear(2017));
