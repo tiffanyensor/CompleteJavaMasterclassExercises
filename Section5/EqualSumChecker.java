10a88e35ca702a503ab7dea3f4b36545b24a0305
tiffanyensor <tiffany_ensor@hotmail.com>
2018-07-11 12:20:31 -0400

    Update and rename Main.java to EqualSumChecker.java

--- Section5/EqualSumChecker.java
+++ Section5/EqualSumChecker.java
@@ -1,6 +1,13 @@
+/* 
+Write a method hasEqualSum with 3 parameters of type int.
+The method should return boolean and it needs to return true if sum of first and second parameter is equal to third parameter.
+Otherwise return false.
+*/
+
+
 package com.company;
 
-public class Main {
+public class EqualSumChecker {
 
     public static void main(String[] args) {
         System.out.println(hasEqualSum(1,1,1));
